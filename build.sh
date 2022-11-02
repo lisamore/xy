dfd11b615d4a6d967d940e4f758b98263f0f8e86
lisamore <117285138+lisamore@users.noreply.github.com>
2022-11-02 08:28:10 -0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+latest_version=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | sed 'y/,/\n/' | grep 'tag_name' | awk -F '"' '{print substr($4,2)}')
+test -f x && current_version=$(./x version | awk 'NR==1 {print $2}')) || current_version='not found'
+if [[ ${latest_version} == ${current_version} ]]
+then
+    echo 'there is nothing to do'
+else
+    git config --local user.name 'GitHub Action'
+    git config --local user.email 'action@github.com
+    git clone https://github.com/XTLS/Xray-core.git 
+    cd Xray-core && go mod download
+    CGO_ENABLED=0 go build -o x -trimpath -ldflags "-s -w -buildid=" ./main
+    mv x ../
+    rm -rf ../Xray-core
+    git commit -am ${latest_version}
+    git push -v --progress
+fi
